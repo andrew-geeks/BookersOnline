98ceb4ee303853892b003438f2b1bc295f0e832f
Andrew George Issac <andrewgeorgeissac@gmail.com>
2022-01-10 22:49:45 +0530

    backend connection

--- src/App.js
+++ src/App.js
@@ -1,14 +1,43 @@
-import React from "react"
+import React,{Component} from "react"
 
 //import './App.css';
 import Navbar from "./navBar";
 
-function App() {
-  return (
-    <div className="App">
-      <Navbar />
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { apiResponse: "" };
+  }
+
+  callAPI() {
+      fetch("http://localhost:9000/home")
+          .then(res => res.text())
+          .then(res => this.setState({ apiResponse: res }));
+  }
+
+  componentWillMount() {
+      this.callAPI();
+  }
+
+  render(){
+    return(
+      <div>
+        <Navbar/>
+        <p>{this.state.apiResponse}</p>
+      </div>
+    )
+  }
+
 }
+
+
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <Navbar />
+//     </div>
+//   );
+// }
 
 export default App;
