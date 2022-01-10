0b60050e59c9085f9e889f96b94614403dde9629
Andrew George Issac <andrewgeorgeissac@gmail.com>
2022-01-10 13:36:47 +0530

    main

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
+import React from "react"
+
 //import './App.css';
+import Navbar from "./navBar";
 
 function App() {
   return (
     <div className="App">
-      <h1 className="heading">BookersOnline</h1>
+      <Navbar />
     </div>
   );
 }
