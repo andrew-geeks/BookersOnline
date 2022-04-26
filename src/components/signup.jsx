1eb204db91b548434a743d9552431c525f5f01c5
Andrew George Issac <andrewgeorgeissac@gmail.com>
2022-04-26 21:38:34 +0530

    signin

--- src/components/signup.jsx
+++ src/components/signup.jsx
@@ -31,8 +31,11 @@ function Signup(){
             <div className='auth-block'>
                 <form method='post'>
                     <input type="text" name='name' required placeholder='Full Name' size="40" value={formValues.name} onChange={(e) => setFormValues({...formValues, name: e.target.value})}></input>
+                    <br></br>
                     <input type="email" name='email' required placeholder='Mail Id' size="40" value={formValues.email} onChange={(e) => setFormValues({...formValues, email: e.target.value})}></input>
+                    <br></br>
                     <input type="password" name='password' required placeholder='Enter password' size="40" value={formValues.password} onChange={(e) => setFormValues({...formValues, password: e.target.value})}></input>
+                    <br></br>
                     <input type="password" required placeholder='Confirm password' size="40" value={formValues.cpassword} onChange={(e) => setFormValues({...formValues, cpassword: e.target.value})}></input>
                     <button 
                         type='submit'  
@@ -47,12 +50,11 @@ function Signup(){
                     </button>
                 </form>
                 <hr></hr>
-                <div className='auth-btns'>
-                    <button>Google</button>
-                    <button>Facebook</button>
-                </div>
-                
-                
+                <h2>Or</h2>
+            </div>
+            <div className='auth-btns'>
+                    <button><a href="https://www.freepnglogos.com/pics/google-logo" title="Image"><img src="https://www.freepnglogos.com/uploads/google-logo-png/google-logo-png-suite-everything-you-need-know-about-google-newest-0.png" width="30" alt="google logo" /></a></button>
+                    <button><img alt="" src="https://bn1304files.storage.live.com/y4mmGHusx-KkcAh_jjoOxJbNxLTAvS8ZJCi5J46cTlNB5yOUlCAD_TAVJCjtxx_2P3wjxXhEddIjqReIy-nWtmo9eFS1tquLKIpVgGRmM-jGtdocJ3tRgLqNWHikhHjXdxz19464-q26zhMDKRw12hLoiPXzPnOoGlYp1RHlMqJocbojcVNYKWPC4926P4QNw7u?width=2000&height=1400&cropmode=none" width="45" height="35" /></button>
             </div>
         </div>
     )
